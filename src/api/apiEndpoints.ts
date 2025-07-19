4ac58d49b12b719bd01008cd154b68e2f02dde74
Nachiket Galande <nachiketgalande1609@gmail.com>
2025-07-19 10:36:15 +0530

    Created dot env

--- src/api/apiEndpoints.ts
+++ src/api/apiEndpoints.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL;
 
 export const fetchDietPlanURL = `${API_BASE_URL}/diet`;
 export const markMealCompleteURL = `${API_BASE_URL}/diet/complete`;
